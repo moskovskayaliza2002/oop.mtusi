f07d0f2ea71faf9ac8a07ec05933698ec42f112a
moskovskayaliza2002 <moscovskayaliza@gmail.com>
2020-10-18 17:21:40 +0300

    Все таски сданы, подгружена 2 лр

--- laba2/src/laba2/Point3d.java
+++ laba2/src/laba2/Point3d.java
@@ -1,45 +1,23 @@
 package laba2;
 
-public class Point3d {
-    // ** координата X **
-    private double xCoord;
-    // ** координата Y **
-    private double yCoord;
-    // ** координата Z **
+public class Point3d extends Point2d {
+    // координата Z
     private double zCoord;
-    // ** Конструктор инициализации **
-    public Point3d ( double x, double y, double z) {
-        xCoord = x;
-        yCoord = y;
+    //  Конструктор инициализации
+    public Point3d (double x, double y, double z) {
+        super(x,y);
         zCoord = z;
     }
-    // ** Конструктор по умолчанию **
+    //  Конструктор по умолчанию
     public Point3d () {
-    //Вызовите конструктор с двумя параметрами и определите источник.
         this(0, 0, 0);
     }
-    // ** Возвращение координаты X **
-    public double getX () {
-        return xCoord;
-    }
-    // ** Возвращение координаты Y **
-    public double getY () {
-        return yCoord;
-    }
-    // ** Возвращение координаты Z **
+    //  Возвращение координаты Z
     public double getZ () {
         return zCoord;
     }
-    // ** Установка значения координаты X **
-    public void setX ( double val) {
-        xCoord = val;
-    }
-    // ** Установка значения координаты Y **
-    public void setY ( double val) {
-        yCoord = val;
-    }
-    // ** Установка значения координаты Z **
-    public void setZ ( double val) {
+    //  Установка значения координаты Z
+    public void setZ (double val) {
         zCoord = val;
     }
     // ** Метод сравнения **
