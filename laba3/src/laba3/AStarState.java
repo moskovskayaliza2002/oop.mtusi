c2b14a7beab6e193188e981f8fe378c3618ae6f5
moskovskayaliza2002 <moscovskayaliza@gmail.com>
2020-11-23 22:18:29 +0300

    60 + 3 full

--- laba3/src/laba3/AStarState.java
+++ laba3/src/laba3/AStarState.java
@@ -1,20 +1,13 @@
 package laba3;
-/**
- * This class stores the basic state necessary for the A* algorithm to compute a
- * path across a map.  This state includes a collection of "open waypoints" and
- * another collection of "closed waypoints."  In addition, this class provides
- * the basic operations that the A* pathfinding algorithm needs to perform its
- * processing.
- **/
+import java.util.HashMap;
 public class AStarState
 {
-    /** This is a reference to the map that the A* algorithm is navigating. **/
     private Map2D map;
+    HashMap<Location,Waypoint>OpenWp=new HashMap<>();
+    // HashMap<Ключ(координата),Значение(вершина-(набор параметров))>открыт.вершина
+    HashMap<Location,Waypoint>CloseWp=new HashMap<>();
+    // HashMap<Ключ(координата),Значение(вершина-(набор параметров))>закрыт.вершина
 
-
-    /**
-     * Initialize a new state object for the A* pathfinding algorithm to use.
-     **/
     public AStarState(Map2D map)
     {
         if (map == null)
@@ -23,63 +16,69 @@ public class AStarState
         this.map = map;
     }
 
-    /** Returns the map that the A* pathfinder is navigating. **/
     public Map2D getMap()
     {
         return map;
     }
-
-    /**
-     * This method scans through all open waypoints, and returns the waypoint
-     * with the minimum total cost.  If there are no open waypoints, this method
-     * returns <code>null</code>.
-     **/
+    // Эта функция должна проверить все вершины в наборе открытых вершин,
+    // и после этого она должна вернуть ссылку на вершину с наименьшей общей стоимостью.
     public Waypoint getMinOpenWaypoint()
     {
-        // TODO:  Implement.
-        return null;
+        if (OpenWp.size()==0)
+            return null;
+        else
+        {
+            Waypoint point=null;
+            float totalCost=100000; // лок. переменная
+            for (Location key: OpenWp.keySet()) // метод получения множества ключей
+            {
+                Waypoint point_cur=OpenWp.get(key);
+                System.out.println("value="+point_cur); // метод получения значения по ключу
+                if (point_cur.getTotalCost()<totalCost)
+                {
+                    totalCost=point_cur.getTotalCost();
+                    point = point_cur;
+                }
+            }
+            return point;
+        }
     }
-
-    /**
-     * This method adds a waypoint to (or potentially updates a waypoint already
-     * in) the "open waypoints" collection.  If there is not already an open
-     * waypoint at the new waypoint's location then the new waypoint is simply
-     * added to the collection.  However, if there is already a waypoint at the
-     * new waypoint's location, the new waypoint replaces the old one <em>only
-     * if</em> the new waypoint's "previous cost" value is less than the current
-     * waypoint's "previous cost" value.
-     **/
+    // Это функция либо добавляет новую точку, либо ищет наименьшую
     public boolean addOpenWaypoint(Waypoint newWP)
     {
-        // TODO:  Implement.
+        if ((!OpenWp.containsKey(newWP.loc)))// если новой вершины нет - добавляем
+            OpenWp.put(newWP.loc,newWP);
+        else // если есть такая вершина
+        {
+            Waypoint point = OpenWp.get(newWP.loc);
+            if(point.getPreviousCost() > newWP.getPreviousCost()) // если новая цена меньше - заменяем точку на новую
+            {
+                OpenWp.put(newWP.loc,newWP);
+                return true;
+            }
+        }
         return false;
     }
 
-
-    /** Returns the current number of open waypoints. **/
+    // Этот метод возвращает количество точек в наборе открытых вершин
     public int numOpenWaypoints()
     {
-        // TODO:  Implement.
-        return 0;
+        return OpenWp.size();
     }
-
-
-    /**
-     * This method moves the waypoint at the specified location from the
-     * open list to the closed list.
-     **/
+    // Эта функция перемещает вершину из набора «открытых вершин» в набор «закрытых вершин».
     public void closeWaypoint(Location loc)
     {
-        // TODO:  Implement.
+        Waypoint point = OpenWp.get(loc); // метод получения значения (вершина) по ключу (местоположение)
+        OpenWp.remove(loc); // удаление элемента из коллекции
+        CloseWp.put(loc,point); // добавление пары (ключ и значение) в коллекцию
     }
-
-    /**
-     * Returns true if the collection of closed waypoints contains a waypoint
-     * for the specified location.
-     **/
+    // Эта функция должна возвращать значение true,
+    // если указанное местоположение встречается в наборе закрытых вершин, и false в противном случае
     public boolean isLocationClosed(Location loc)
     {
-        // TODO:  Implement.
-        return false;
+        if (CloseWp.containsKey(loc)) // метод проверки наличия ключа в коллекции
+            return true;
+        else
+            return false;
     }
 }
--- laba3/src/laba3/Location.java
+++ laba3/src/laba3/Location.java
@@ -1,16 +1,8 @@
 package laba3;
-/**
- * This class represents a specific location in a 2D map.  Coordinates are
- * integer values.
- **/
 public class Location
 {
-    /** X coordinate of this location. **/
     public int xCoord;
-
-    /** Y coordinate of this location. **/
     public int yCoord;
-
 
     /** Creates a new location with the specified integer coordinates. **/
     public Location(int x, int y)
@@ -24,7 +16,27 @@ public class Location
     {
         this(0, 0);
     }
-    public boolean equals(Object obj) {
-        return (this == obj);
+
+    public int hashCode() // генерация целочисленного кода объекта
+    {
+        //генерация методом Джошуа Блоха, используем свои члены класса
+        int result = 17;
+        result = 37 * result + xCoord;
+        result = 37 * result + yCoord;
+        return result;
+    }
+
+    public boolean equals (Object obj) //сравнение объектов или ссылки (если одинаковые - вернет true)
+    {
+        if (obj == this)
+        {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass())
+        {
+            return false;
+        }
+        Location loc = (Location)obj;
+        return loc.xCoord == this.xCoord & loc.yCoord == this.yCoord;
     }
 }
